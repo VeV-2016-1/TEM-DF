e6a6f7a06573f6a7a2b490b098dec5a6573d0428
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-21 11:24:36 -0300

    Testing get a medic by a schedule

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ScheduleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "if get a medic by a schedule" do
+      medics = Schedule.where(id:980190962).select('distinct medic_id')
+      medic = medics[0]
+
+    assert_equal medic.medic_id, 64810937
+  end
 end
