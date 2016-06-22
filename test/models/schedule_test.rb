1be5914bd3df4d82f774ef93182e0e33e89498bb
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-21 21:42:38 -0300

    Read a schedule id

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class ScheduleTest < ActiveSupport::TestCase
   test "if get a medic by a schedule" do
+      puts "AQUUUI"
+      puts Schedule.all
+      a = Schedule.all
+      puts a[0].id
       medics = Schedule.where(id:980190962).select('distinct medic_id')
       medic = medics[0]
 
